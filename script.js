f437fe4d6b0b6b8ef58fb657db76ef122955edef
Piriali <Piriali@users.noreply.github.com>
2019-04-26 14:07:42 +0400

    portfolio

---
+++ script.js
@@ -0,0 +1,80 @@
+addColor();
+
+//responsive menu function 
+$('.toggle').on('click', function () {
+	$(this).closest('.header-top').toggleClass('toggle-open');
+	$('header').toggleClass('header-open');
+	$('.open-nav').fadeToggle(1000);
+});
+
+//scroll buttons function 
+var $btnDown = $('.scroll-down');
+$btnDown.on("click", function () {
+	$("html,body").animate({scrollTop: $(".portfolio").offset().top}, 1000);
+});
+var $btnTop = $('.scroll-top');
+$(window).on("scroll", function () {
+	if ($(window).scrollTop() >= 20){
+		$btnTop.fadeIn();
+	}else{
+		$btnTop.fadeOut();
+	}
+});
+$btnTop.on("click", function () {
+	$("html,body").animate({scrollTop:0}, 1000);
+});
+
+
+
+
+//adding differents color over the portfolio item
+function addColor() {
+	var color = [	['rgba(52, 115, 21,0.8)'],
+					['rgba(165, 34, 121,0.8)'],
+					['rgba(255, 160, 0,0.8)'],
+					['rgba(255, 54, 60,0.8)'],
+					['rgba(0, 107, 146,0.8)'],
+					['rgba(200, 81, 50,0.8)']
+				];
+	var i;
+	var j=0;
+	var opacity = document.getElementsByClassName('portfolio-overlay');
+	for(i=0; i<opacity.length;i++){
+		opacity[i].style.backgroundColor = color[j];
+		j++;
+		if(j>=6){
+			j=0;
+		}
+	}
+}
+
+
+// //function of  skill bar animation  
+$(window).on('scroll' , function(){
+    scroll_pos = $(window).scrollTop() + $(window).height();
+    element_pos = $('.skillbar-container').offset().top + $('.skillbar-container').height()/2 ;
+    if (scroll_pos > element_pos) {
+        $(function(){
+    xer();
+})
+	 };
+})
+
+
+
+var xer = function(blah){
+    $('.skillbar').each(function () {
+			$(this).find('.skillbar-bar').animate(
+			{width:$(this).attr('data-percent')}, 2000)
+		});
+}
+
+
+$('.count').each(function () {
+	$(this).prop('Counter',0).animate(
+	{Counter:$(this).text()},
+	{duration: 2000,
+	 easing: 'swing',
+	 step:function(now){$(this).text(Math.ceil(now));}
+	});
+});
---
+++ seeBtn.js
@@ -0,0 +1,95 @@
+
+seeBtn('start');
+
+//changing showing quantity of blocks, if devise screen  size is changing after open page    
+window.onresize = function(){ 
+	var item = document.getElementsByClassName('portfolio-item');
+	for(i=0; i<item.length; i++){
+		item[i].style.display="none";
+	}
+	seeBtn('start'); 
+}
+function seeBtn(show){
+   var ADDQTY; // how many items will open out and hide on the onclick event "see more" , "see less"
+   var itemQty; // quantity of default showing blocks(items)
+   var item = document.getElementsByClassName('portfolio-item');
+   var less = document.getElementById('less');
+   var more = document.getElementById('more');
+   var openItemQty = 0; //quantity of opened blocks(items)
+   var i;
+   var j;
+
+   	// changing value of ADDQTY and itemQty, by the device screen resizing
+	if (window.matchMedia('screen and (min-width: 1199px)').matches){
+		itemQty = 6;
+		ADDQTY=3;
+	} else if (window.matchMedia('screen and (min-width: 767px) and (max-width: 1199px) ').matches) {
+	   itemQty = 4;
+	   ADDQTY=2;
+	} 
+	else if (window.matchMedia('screen and (max-width: 767px)').matches){
+		itemQty = 2;
+		ADDQTY=1;
+	}
+	var addQty = ADDQTY;
+	
+
+	// quantity of showing block when paged opened 
+	if( show === 'start' ){
+		for(i=0; i<item.length; i++){
+			if(item[i].style.display === 'none' || item[i].style.display === ''){
+				for(j=i; itemQty>0 && j<item.length; j++){
+					itemQty--;
+					item[j].style.display="block";
+					i = item.length+1;
+				}  
+			}
+		}
+	}
+	// opening  blocks on clicking "see more" button
+	if(show === 'more'){
+		for(i=0; i<item.length; i++){
+			if(item[i].style.display === 'none' || item[i].style.display === ''){
+				for(j=i; addQty >0 && j<item.length; j++){
+					item[j].style.display="block";
+					addQty--;
+					i = item.length-1;
+				}  	
+			}
+		}
+	}
+
+	// opening  hide on clicking "see less" button
+	if(show === 'less'){
+		for(i=item.length-1; i>=0; i--)
+			if(item[i].style.display === 'block'){
+				for(j=i; addQty>0 && j>=ADDQTY*2; j--){
+					item[j].style.display="none";
+					addQty--;
+					i=-1;
+				}  
+			}	
+	}
+	// how many blocks are open on the current time 
+	for(i=0; i<item.length; i++){
+		if(item[i].style.display === 'block'){
+			openItemQty=openItemQty+1;
+		}
+	}
+
+	//open out - hide "see less" button
+	if(openItemQty>ADDQTY*2){
+		less.style.display="inline-block";
+	}else if(openItemQty<=ADDQTY*2){
+		less.style.display="none";
+	}
+
+	//open out - hide "see more" button
+	if(openItemQty > item.length -1){
+		more.style.display="none";
+	}else{
+		more.style.display="inline-block";
+	}
+	
+
+}
